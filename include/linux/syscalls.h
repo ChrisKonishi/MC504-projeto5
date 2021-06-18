0d07f2dc512e2f805c0446d2b0716bd2c2392bdf
Chris Konishi <christian.konishi@gmail.com>
2021-06-18 11:42:50 -0300

    add syscall signature to syscalls.h

--- include/linux/syscalls.h
+++ include/linux/syscalls.h
@@ -1264,6 +1264,9 @@ asmlinkage long sys_mmap_pgoff(unsigned long addr, unsigned long len,
 			unsigned long fd, unsigned long pgoff);
 asmlinkage long sys_old_mmap(struct mmap_arg_struct __user *arg);
 
+/* Project 5 signatures */
+asmlinkage long getuserweight(int uid);
+asmlinkage long setuserweight(int uid, int weight);  
 
 /*
  * Not a real system call, but a placeholder for syscalls which are
